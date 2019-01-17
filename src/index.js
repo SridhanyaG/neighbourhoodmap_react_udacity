3c917ccf5027f7cf4e2d2fe2272bed86a5bb7d34
Ganapathi, Sridhanya <sridhanya.ganapathi@team.neustar>
2019-01-17 14:30:10 +0530

    Service worker fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './views/App'
 import './index.css'
 import './views/include/bootstrap'
+import registerServiceWorker from './registerServiceWorker';
 
 import { BrowserRouter } from 'react-router-dom';
 import 'font-awesome/css/font-awesome.min.css';
@@ -11,3 +12,4 @@ import 'font-awesome/css/font-awesome.min.css';
 ReactDOM.render(<BrowserRouter>
                   <App />
                 </BrowserRouter>, document.getElementById('root'))
+registerServiceWorker();
